bf708d00460deaf172e2e25b1b4c386cf7b17811
inkms <inkms12@gmail.com>
2018-10-21 13:10:31 +0200

    Fixed naming

--- PlotPositions.m
+++ PlotPositions.m
@@ -1,11 +1,11 @@
 %Plot everything - takes forever with the array of structs approach
 hold on;
-scatter3(S.position(1,1),S.position(1,2),S.position(1,3),100,'filled','g');
+scatter3(server.pos(1),server.pos(2),server.pos(3),100,'filled','g');
 for i=1:nc
-    scatter3(cluster(i).position(1),cluster(i).position(2),cluster(i).position(3),30,'b');
+    scatter3(cluster(i).pos(1),cluster(i).pos(2),cluster(i).pos(3),30,'b');
 end
 for i=1:nt
-    scatter3(nodes(i).position(1),nodes(i).position(2),nodes(i).position(3),3,'r');
+    scatter3(node(i).pos(1),node(i).pos(2),node(i).pos(3),3,'r');
 end
 view(40,35)
     
