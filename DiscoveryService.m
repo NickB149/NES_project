d1273ccea600db7e3289ff74a244fe8abc1b1573
inkms <44236455+inkms@users.noreply.github.com>
2018-10-21 12:10:56 +0200

    Small aesthetical improvements

--- DiscoveryService.m
+++ DiscoveryService.m
@@ -15,6 +15,32 @@ for i=1:nt
     end
 end
 
+
+
+%%
+%Choose one clusterhead at random
+for i=1:nt
+    index=randi([2,node(i).clustersinrange(1)]);
+    node(i).clusterID=node(i).clustersinrange(index);
+    node(i).clustersinrange(1)=[];
+end
+%%
+cluster(nc).nodesreg(1)=1;
+for i=1:nc-1
+    cluster(i).nodesreg(1)=1;
+end
+%Clusters get list of assigned nodes
+for i=1:nt
+    cluster(node(i).clusterID).nodesreg(cluster(node(i).clusterID).nodesreg(1)+1)=i;
+    cluster(node(i).clusterID).nodesreg(1)=cluster(node(i).clusterID).nodesreg(1)+1;
+end
+%The following line removes the first column of the matrix, which is used
+%in the previous lines to store the number of tags linked to each
+%clusterhead. If you want to make use of it, comment it.
+for i=1:nc
+    cluster(i).nodesreg(1)=[];
+end
+%%
 %Get clusterheads in range of clusterheads
 
 for i=1:nc
@@ -43,34 +69,8 @@ server.clustersinrange(1)=[];
 for i=1:nc
     distance=norm(cluster(i).pos(:)-server.pos(:));
     if distance<cluster(i).range && distance<server.range
-        cluster(i).pathtoserver=server;
+        cluster(i).pathtoserver=[cluster(i),server];
     else
-        cluster(i).pathtoserver=[cluster(min(cluster(i).clustersinrange)),cluster(min(cluster(i).clustersinrange)).pathtoserver];
+        cluster(i).pathtoserver=[cluster(i),cluster(min(cluster(i).clustersinrange)),cluster(min(cluster(i).clustersinrange)).pathtoserver(2:end)];
     end
 end
-
-%%
-%Choose one clusterhead at random
-for i=1:nt
-    index=randi([2,node(i).clustersinrange(1)]);
-    node(i).clusterID=node(i).clustersinrange(index);
-    node(i).clustersinrange(1)=[];
-end
-%%
-cluster(nc).nodesreg(1)=1;
-for i=1:nc-1
-    cluster(i).nodesreg(1)=1;
-end
-%Clusters get list of assigned nodes
-for i=1:nt
-    cluster(node(i).clusterID).nodesreg(cluster(node(i).clusterID).nodesreg(1)+1)=i;
-    cluster(node(i).clusterID).nodesreg(1)=cluster(node(i).clusterID).nodesreg(1)+1;
-end
-%The following line removes the first column of the matrix, which is used
-%in the previous lines to store the number of tags linked to each
-%clusterhead. If you want to make use of it, comment it.
-for i=1:nc
-    cluster(i).nodesreg(1)=[];
-end
-
-
--- NodeGeneration.m
+++ NodeGeneration.m
@@ -1,5 +1,3 @@
-
-
 clear all
 close all
 clc
@@ -15,14 +13,19 @@ range_of_nodes=10;%in meters
 range_of_server=50;%in meters
 %%
 %Structs initialization
+cluster(nc).ID=nc;
+cluster(nc).type='cluster';
 cluster(nc).pos(3)=0;
 cluster(nc).range=range_of_clusterheads;
 cluster(nc).nodesreg=0;
 
-node(nt).clustersinrange=0;
-node(nt).clusterID=0;
+node(nt).ID=nt;
+node(nt).type='node';
 node(nt).pos(3)=0;
 node(nt).range=range_of_nodes;
+node(nt).clustersinrange=0;
+node(nt).clusterID=0;
+
 %%
 %Ranges
 for i=1:nc
@@ -32,16 +35,16 @@ for i=1:nt
     node(i).range=random('normal',range_of_nodes,0.2);
 end
 %%
-%Coordinators generation
+%Clusterheads generation
 gridy=nc/gridx;
 z=3;
 x=dis/2;
 y=dis/2;
 for i=1:gridx
     for j=1:gridy
-        cluster((i-1)*gridy+j).pos=[x,y,z];
-        cluster((i-1)*gridy+j).type='cluster';
         cluster((i-1)*gridy+j).ID=(i-1)*gridy+j;
+        cluster((i-1)*gridy+j).type='cluster';
+        cluster((i-1)*gridy+j).pos=[x,y,z];
         y=y+dis;
     end
     y=dis/2;
