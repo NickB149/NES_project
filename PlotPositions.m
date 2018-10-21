f23ac8d8cf01846c2be8c8673ba9eb391c970f28
rishabh <rishabh@git-training.com>
2018-10-21 13:31:09 +0200

    PlotPositions - fixed the issues caused by stashing

--- PlotPositions.m
+++ PlotPositions.m
@@ -4,6 +4,7 @@ scatter3(server.pos(1),server.pos(2),server.pos(3),100,'filled','g');
 for i=1:nc
     scatter3(cluster(i).pos(1),cluster(i).pos(2),cluster(i).pos(3),30,'b');
 end
+
 for i=1:nt
     scatter3(node(i).pos(1),node(i).pos(2),node(i).pos(3),3,'r');
 end
