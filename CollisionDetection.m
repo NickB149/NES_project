04593a726223885eae60af6b43875663c4b6a512
inkms <inkms12@gmail.com>
2018-10-21 15:34:40 +0200

    Region detection in CollisionDetection.m

--- CollisionDetection.m
+++ CollisionDetection.m
@@ -1,6 +1,6 @@
-clear all
-close all
-clc
+%clear all
+%close all
+%clc
 
 %% Generate test input data
 % Create nodes
@@ -16,8 +16,10 @@ for j = 1 : nNodes/2
     Comm(j).ID = j;
     Comm(j).type = 'A';
     Comm(j).sender = node(j);
+    Comm(j).senderID = node(j).ID; %Delete, only for debugging
     Comm(j).receiver = [node(j+nNodes/2)];
     Comm(j).collision = [];
+    Comm(j).region=[];
     
     Comm(j).latency = 0;
     Comm(j).time = 0;
@@ -30,8 +32,10 @@ for j = nNodes/2 : nNodes/2+5
     Comm(j).ID = nNodes/2 + j;
     Comm(j).type = 'B';
     Comm(j).sender = node(j);
+    Comm(j).senderID = node(j).ID; %Delete, only for debugging
     Comm(j).receiver = [node(j-1), node(j-2)];
     Comm(j).collision = [];
+    Comm(j).region=[];
     
     Comm(j).latency = 0;
     Comm(j).time = 0;
@@ -70,7 +74,51 @@ for i = 1 : size(Comm,2)
         end
     end
 end
-
+%% Iñaki Addition, check region, collision accessing medium by 2 senders
+% Iterate over receivers
+for i = 1 : size(Comm,2)
+    % Iterate over senders
+    for j = 1 : size(Comm,2)
+        % Ignore sender when its the same message
+        if(i ~= j)
+            % Test whether there is a collision
+            % If there is a collision, attach ID of sender node that
+            % creates this collision
+            if (distance(Comm(i).sender,Comm(j).sender) <= Comm(i).sender.range || distance(Comm(i).sender,Comm(j).sender) <= Comm(j).sender.range )
+                % Check if sender ID is not already in the list
+                if (ismember(Comm(j).sender.ID,Comm(i).region) == 0)
+                    Comm(i).region = [Comm(i).region,Comm(j).sender.ID];
+                end               
+            end
+        end
+    end
+end
+%%
+%Modify region column so that all the comms that collide in the same
+%region are identified with the same region list
+for i=1:size(Comm,2)
+    Comm(i).region=[Comm(i).sender.ID, Comm(i).region];
+    changed=1;
+    while (changed)
+        currsize=size(Comm(i).region,2);
+        for j=1:size(Comm,2)
+            for k=1:currsize
+                if (ismember(Comm(i).region(k),Comm(j).region))
+                    Comm(i).region=[Comm(i).region,Comm(j).region];
+                end
+            end
+            Comm(i).region=unique(Comm(i).region);
+        end
+        if (size(Comm(i).region)~=currsize)
+            changed=1;
+        else
+            changed=0;
+        end
+        
+    end
+end
+            
+                
 % Function that computes the Euclidian distance between two nodes 
 function d = distance(n1, n2)
     d = norm(n1.pos - n2.pos);
